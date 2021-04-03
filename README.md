a8d67fdc66f0656837ad6754614827f69ab2ff94
gokulkrishh <krishnangokul9@gmail.com>
2021-04-03 08:54:16 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# buttondown-subscribers-count
+A simple API to get the count of subscribers
---
+++ api/subscribers.js
@@ -0,0 +1,36 @@
+const fetch = require('node-fetch');
+
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader('Cache-Control', 's-maxage=86400');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
+module.exports = allowCors((_, res) => {
+  const { BUTTONDOWN_API_KEY = '' } = process.env;
+
+  const options = {
+    headers: {
+      Authorization: `Token ${BUTTONDOWN_API_KEY}`,
+    },
+  };
+
+  fetch(`https://api.buttondown.email/v1/subscribers`, options)
+    .then((response) => response.json())
+    .then(({ count = '90+' }) => {
+      res.json({ total: count });
+    })
+    .catch((error) => {
+      res.json({ total: 0, message: 'Error occuried while fetching the subscribers count.', error });
+    });
+});
